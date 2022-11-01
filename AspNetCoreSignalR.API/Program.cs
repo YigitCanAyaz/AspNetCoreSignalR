df3f6a9c7b8ca134a454dd1444c3cb85715f4976
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-11-01 21:37:11 +0300

    Working with SignalR + added signalr.js + interactive code added to Index.cshtml

--- AspNetCoreSignalR.API/Program.cs
+++ AspNetCoreSignalR.API/Program.cs
@@ -5,10 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSignalRCore();
+builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("https://localhost:7230").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -20,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
