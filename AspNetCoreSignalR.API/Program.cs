cde2c3daab947d634122bdb4b45a880627afb515
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-11-02 03:10:34 +0300

    Created Team, User and DbContext settings

--- AspNetCoreSignalR.API/Program.cs
+++ AspNetCoreSignalR.API/Program.cs
@@ -1,4 +1,6 @@
 using AspNetCoreSignalR.API.Hubs;
+using AspNetCoreSignalR.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,11 @@ builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConStr"]);
+});
 
 builder.Services.AddCors(options =>
 {
