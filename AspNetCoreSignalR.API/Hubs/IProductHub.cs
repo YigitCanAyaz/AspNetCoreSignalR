de25d3f4593b19f7de18af1b31c218fde62b24de
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-11-04 16:44:32 +0300

    Created strongly typed hub for product

---
+++ AspNetCoreSignalR.API/Hubs/IProductHub.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCoreSignalR.API.Models;
+
+namespace AspNetCoreSignalR.API.Hubs
+{
+    public interface IProductHub
+    {
+        Task ReceiveProduct(Product p);
+    }
+}
---
+++ AspNetCoreSignalR.API/Hubs/ProductHub.cs
@@ -0,0 +1,13 @@
+﻿using AspNetCoreSignalR.API.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AspNetCoreSignalR.API.Hubs
+{
+    public class ProductHub : Hub<IProductHub>
+    {
+        public async Task SendProduct(Product p)
+        {
+            await Clients.All.ReceiveProduct(p);
+        }
+    }
+}
